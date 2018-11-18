197eb29380e29aed8cc0c657a25a3f490f82029f
abhishekbhardwaj96 <31645862+abhishekbhardwaj96@users.noreply.github.com>
2018-11-18 20:11:46 +0530

    Update algo 5.cpp

--- algo 5.cpp
+++ algo 5.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 bool isSubString(string str,string substr){
+    	/*
+    	Objective : To check if the subseqyence entered occurs in sequence inputted in O(m+n) 
+    	Input :     str:-string that is n no of events
+    		    substr;- substring that is to be search in a string
+	Return values : None 
+   	 */
+	
 	int len,sublen,i,j=0,temp=0;
 	len = str.length();
 	sublen = substr.length();
@@ -11,21 +18,22 @@ bool isSubString(string str,string substr){
 		if(str[i]==substr[j]){								// if character matches
 			j++;
 		}
-		else{												//temp is a variable that is used to store a positon example:- if buy flip is in string and we 
-			j = temp;										// have buy flipkart in substring then k will not match and searching has to be strted from 
-		}													//start of the word buy flipkart
+		else{										//temp is a variable that is used to store a positon example:- if buy flip is in string and we 
+			j = temp;								// have buy flipkart in substring then k will not match and searching has to be strted from 
+		}										//start of the word buy flipkart
 		if(substr[i] == ','){
 			temp = j+1;
 		}
 		
 	}
-	if(j == sublen&& j!=0){								   //if j is equal to sublen means we have reach at the end of substr
+	if(j == sublen&& j!=0){						            	   //if j is equal to sublen means we have reach at the end of substr
 		return true;
 	}
 	return false;
 }
 
 int main(){
+  	
 	string str,substr;
 	cout<<"enter n  events separated by ',' \n";
 	getline(cin,str);
